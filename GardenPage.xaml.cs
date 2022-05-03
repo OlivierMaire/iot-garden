ac4f51f052f563fd6ae8405f6fc9236482762e6b
Olivier <olivier.maire.jp@gmail.com>
2022-05-04 01:59:10 +0900

    add main page

---
+++ GardenPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace iot_garden;
+
+public partial class GardenPage : ContentPage
+{
+	int count = 0;
+
+	public GardenPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+		CounterLabel.Text = $"Current count: {count}";
+
+		SemanticScreenReader.Announce(CounterLabel.Text);
+	}
+}
+
